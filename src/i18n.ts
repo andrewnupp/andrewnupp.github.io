c0fc5ee6ceeb3359df90db1090231789d7708a4c
andrewnupp <andrewnupp@gmail.com>
2026-02-21 18:25:58 -0500

    setup language switching and tab title

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,21 +1,22 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 // Import JSON files
 import en from '../src/en.json';
 import ja from '../src/ja.json';
 
 i18n
+  .use(LanguageDetector)
   .use(initReactI18next)
   .init({
     resources: {
       en: { translation: en },
       ja: { translation: ja },
     },
-    lng: 'en', // default language
     fallbackLng: 'en',
     interpolation: {
-      escapeValue: false, // React already protects from XSS
+      escapeValue: false,
     },
     returnObjects: true 
   });
